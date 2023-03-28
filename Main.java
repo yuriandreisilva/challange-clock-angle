994b1c8107c3e9a1e9d9634df15ec00f488263d6
Yuri Andrei da Silva <yuri_andrei@hotmail.com>
2023-03-28 10:21:47 -0300

    feat: adding validation to input hour and minute

--- Main.java
+++ Main.java
@@ -6,17 +6,27 @@ public class Main {
         int minute;
         int result;
 
-        Scanner read = new Scanner(System.in);
         Clock clock = new Clock();
+        Scanner read = new Scanner(System.in);
 
         System.out.printf("Informe um valor para hora: \n");
         hour = read.nextInt();
 
+        while (hour < 0 || hour > 12) {
+            System.out.println("Por favor, informe um valor entre 0 e 12, para hora: ");
+            hour = read.nextInt();
+        }
+
         System.out.printf("Informe um valor para minuto: \n");
         minute = read.nextInt();
 
+        while (minute < 0 || minute > 60) {
+            System.out.println("Por favor, informe um valor entre 0 e 60, para minuto: ");
+            minute = read.nextInt();
+        }
+
         result = clock.returnClockAngle(hour, minute);
 
-        System.out.printf("O ângulo para a hora é: " + result);
+        System.out.printf("O ângulo para a hora é: " + result + "º graus.");
     }
 }
